5f81d132a4996c905ba7e2cd9985dfa55287d051
andreaDiClaudio <andreadicla@gmail.com>
2023-02-12 11:25:43 +0100

    Implemented GET methods (get all, get by id)

--- 01.1._Birds_REST_API/app.js
+++ 01.1._Birds_REST_API/app.js
@@ -4,8 +4,46 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+//Represents table in the database
+const birds = [];
+
+//Creation of 'bird' objects
+const birdOne = {
+  id: 1,
+  name: "Bird One",
+  classification: "Parrot",
+};
+
+const birdTwo = {
+  id: 2,
+  name: "Bird Two",
+  classification: "Blue Jay",
+};
+
+const birdThree = {
+  id: 3,
+  name: "Bird Three",
+  classification: "Owl",
+};
+
+const birdFour = {
+  id: 4,
+  name: "Bird Four",
+  classification: "Toucan",
+};
+
+//Populating the 'table'
+birds.push(birdOne, birdTwo, birdThree, birdFour);
+
+//HTTP Methods
 app.get("/birds", (req, res) => {
-  res.send({ message: "Hello World!" });
+  res.send(birds);
+});
+
+app.get("/birds/:id", (req, res) => {
+  const requestedPosition = req.params.id - 1;
+  const birdById = birds.at(requestedPosition);
+  res.send({ birdById });
 });
 
 app.listen(8080);
