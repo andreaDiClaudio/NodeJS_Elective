b0a377796e44d1bf8b44aa73c4def76bab34cf2c
andreaDiClaudio <andreadicla@gmail.com>
2023-02-14 12:49:15 +0100

    Added More endpoints; Update the API Design

--- 01.1._Birds_REST_API/app.js
+++ 01.1._Birds_REST_API/app.js
@@ -4,31 +4,31 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-//Represents table in the database
+//Represents the table in the database
 const birds = [];
 
 //Creation of 'bird' objects
 const birdOne = {
   id: 1,
-  name: "Bird One",
+  name: "Andrea",
   classification: "Parrot",
 };
 
 const birdTwo = {
   id: 2,
-  name: "Bird Two",
-  classification: "Blue Jay",
+  name: "Nicolas",
+  classification: "Pigeon",
 };
 
 const birdThree = {
   id: 3,
-  name: "Bird Three",
+  name: "Anders",
   classification: "Owl",
 };
 
 const birdFour = {
   id: 4,
-  name: "Bird Four",
+  name: "Jone",
   classification: "Toucan",
 };
 
@@ -36,14 +36,33 @@ const birdFour = {
 birds.push(birdOne, birdTwo, birdThree, birdFour);
 
 //HTTP Methods
+//GET all
 app.get("/birds", (req, res) => {
   res.send(birds);
 });
 
+//GET by id
 app.get("/birds/:id", (req, res) => {
   const requestedPosition = req.params.id - 1;
   const birdById = birds.at(requestedPosition);
+
   res.send({ birdById });
+});
+
+//GET by name
+app.get("/birds/name/:birdName", (req, res) => {
+  const found = birds.find(
+    (bird) => bird.name.toLowerCase() == req.params.birdName.toLowerCase());
+
+  res.send({ found });
+});
+
+//GET by classification
+app.get("/birds/classification/:classification", (req, res) => {
+  const found = birds.find(
+    (bird) => bird.classification.toLowerCase() == req.params.classification.toLowerCase());
+
+  res.send({ found });
 });
 
 //Port
