16e603cd3198024a5acb198c67da5ad59910b918
andreaDiClaudio <andreadicla@gmail.com>
2023-02-17 13:39:49 +0100

    Exercises

--- 00._Miscellaneous/extra_curricular/loops/spaceships.js
+++ 00._Miscellaneous/extra_curricular/loops/spaceships.js
@@ -1,7 +1,35 @@
-const spaceship = [
-    { name: "Chinese Weather Baloon"},
-    {name: undefined, type: "UFO"},
-    {name: "Apollo 13", versionNumber: 13}
+const spaceships = [
+    {name: "Chinese Weather Baloon", isPriceless: false, cost: 1_000_000},
+    {name: undefined, type: "UFO", isPriceless: true, cost: Number.MAX_SAFE_INTEGER},
+    {name: "Apollo 13", versionNumber: 13, isPriceless: false, cost: 2_000_000}
 ];
 
-spaceship.forEach(spaceship => console.log(spaceship))
+/*
+forEach is never ther ight one
+spaceship.forEach((spaceship, index, array) => console.log(array));
+*/
+
+//task for all spaceship make the owner China
+const spaceshipsWithOwner = spaceships.map(spaceship => {
+    spaceship.owner = "China";
+    return {...spaceship, owner: "China"};//this line does not mutate the original array.
+});
+
+
+//add a cost of "isPriceless: true" to all spaceshipsWithOwner
+/*
+const spaceshipsWithOwnerAndPrice = spaceshipsWithOwner.map(spaceship => {
+    spaceship.isPriceless = true;
+    return spaceship;
+});
+
+console.log(spaceshipsWithOwnerAndPrice);
+*/
+
+//Give me the priceless and not pricesless spaceships into different arrays
+const pricelessShips = spaceships.filter(spaceship => spaceship.isPriceless == true);
+const notPricelessShips = spaceships.filter(spaceship => spaceship.isPriceless == false);
+
+//task find a spaceship that costs less than 2 million money
+const affordableSpaceship = spaceships.find(spaceship => spaceship.cost < 2_000_000);
+console.log(affordableSpaceship);
