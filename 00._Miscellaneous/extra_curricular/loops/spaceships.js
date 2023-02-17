1bc6843c9db53895847d9f3122a8263aeaebbf13
andreaDiClaudio <andreadicla@gmail.com>
2023-02-17 12:53:16 +0100

    Created new folder, introduction to ESLint and loops exercise

---
+++ 00._Miscellaneous/extra_curricular/loops/spaceships.js
@@ -0,0 +1,7 @@
+const spaceship = [
+    { name: "Chinese Weather Baloon"},
+    {name: undefined, type: "UFO"},
+    {name: "Apollo 13", versionNumber: 13}
+];
+
+spaceship.forEach(spaceship => console.log(spaceship))
