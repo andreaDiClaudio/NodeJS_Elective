2b5a75e53bc7b2ddf8e28771201f42d502b2f80c
andreaDiClaudio <andreadicla@gmail.com>
2023-02-17 11:51:23 +0100

    Create frontpage and tanks folder with relative html/js/css

--- 03._Plain_HTML_Server/app.js
+++ 03._Plain_HTML_Server/app.js
@@ -3,9 +3,15 @@
 const express = require("express");
 const app = express()
 
+app.use(express.static("public"));//you are specifying where to find the static files. and allows the client to access the file only inside that folder
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/frontpage.html")
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html")
 });
+
+app.get("/tanks", (req, res) => {
+    res.sendFile(__dirname + "/public/tanks/tanks.html")
+})
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
