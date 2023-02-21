d7c15d2bcac83306bdfdb1c436a712abd131adbb
andreaDiClaudio <andreadicla@gmail.com>
2023-02-21 12:08:32 +0100

    Solved second part of the Birds REST API assigment

--- 02._Birds_REST_API/app.js
+++ 02._Birds_REST_API/app.js
@@ -1,8 +1,13 @@
 "use strict";
 
 const express = require("express");//Importing the module
+const bodyParser = require('body-parser');//Needed for post method
+
 const app = express();//Instanciating the module
+
 app.use(express.json());//For parsing the body as json.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))//Parses all the urlencoded bodies
 
 //Represents the table in the database
 const birds = [];
@@ -44,9 +49,13 @@ birds.push(birdOne, birdTwo, birdThree, birdFour);
 app.get("/", (req, res) => {
   res.send(
     `<h1>Birds Rest API Exercise</h1>
-        <h3>Endpoints:</h3>
-        <p>• /birds</p>
-        <p>• /birds/{id}</p>`
+        <h3>Implemented Endpoints:</h3>
+        <p>• GET - "/birds"</p>
+        <p>• GET - "/birds/{id}"</p>
+        <p>• POST - "/birds"</p>
+        <p>• PUT - "/birds/{id}"</p>
+        <p>• PATCH - "/birds/{id}"</p>
+        <p>• DELETE - "/birds/{id}"</p>`
   );
 });
 
@@ -55,12 +64,61 @@ app.get("/birds", (req, res) => {
   res.send({data: birds});
 });
 
-//GET by id                      | Callback function
+//GET by id                     | Callback function
 app.get("/birds/:id", (req, res) => {
-  const found = birds.find(bird => bird.id === Number(req.params.id));
+  const requestedBird = birds.find(bird => bird.id === Number(req.params.id));
 
-  res.send({data: found})
+  if (!requestedBird) return res.sendStatus(404);
+
+  res.send({data: requestedBird});
 });
+
+//POST 
+app.post("/birds", (req, res) => {
+  const body = req.body;
+
+  const calculateId = birds.length + 1;
+  const newBird = {id: calculateId, ...body};
+  birds.push(newBird);
+
+  res.send({data: newBird});
+})
+
+//PUT 
+app.put("/birds/:id", (req,res) => {
+  const requestedBird = birds.find(bird => bird.id === Number(req.params.id));
+
+  if (!requestedBird) return res.sendStatus(404);
+ 
+  requestedBird.name = req.body.name;
+  requestedBird.maleRating = req.body.maleRating;
+  requestedBird.femaleRating = req.body.femaleRating;
+  
+  res.send({data: requestedBird});
+})
+
+//PATCH
+app.patch("/birds/:id", (req,res) => {
+  const requestedBird = birds.find(bird => bird.id === Number(req.params.id));
+
+  if (!requestedBird) return res.sendStatus(404);
+
+  req.body.hasOwnProperty("name") ? requestedBird.name = req.body.name : requestedBird.name;
+  req.body.hasOwnProperty("maleRating") ? requestedBird.maleRating = req.body.maleRating : requestedBird.maleRating;
+  req.body.hasOwnProperty("femaleRating") ? requestedBird.femaleRating = req.body.femaleRating : requestedBird.femaleRating;
+
+  res.json(requestedBird);
+});
+
+//DELETE
+app.delete("/birds/:id", (req,res) => {
+  const requestedBird = birds.find(bird => bird.id === Number(req.params.id));
+  const birdIndex = birds.indexOf(requestedBird);
+
+  birds.splice(birdIndex, 1);
+
+  res.sendStatus(200);
+})
 
 //Port
 app.listen(8080, () => {
