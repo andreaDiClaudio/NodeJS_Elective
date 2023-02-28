9f4de0a37e298e5b2e5304bfe4528a691e4a2b08
andreaDiClaudio <andreadicla@gmail.com>
2023-02-28 10:59:42 +0100

    Polishing

--- 04._Time_To_Deploy/public/Activities/activities.js
+++ 04._Time_To_Deploy/public/Activities/activities.js
@@ -8,10 +8,7 @@ const addActivityButton = document.getElementById("add-activity-button");
 
 function formatDate(date){
   const stringDate = date.toString();
-
   const arr = stringDate.split("-");
-
-  console.log(arr);
 
   const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
@@ -52,6 +49,7 @@ function addActivity(){
   time.innerText = document.getElementById("time-input").value;
   description.innerText = document.getElementById("description-input").value;
 
+  //appendChild
   dateWrapper.appendChild(date);
   timeWrapper.appendChild(time);
   descriptionWrapper.appendChild(description);
