0d03bb8f00acfb9dc1711cb4320b8ed95632c6cf
andreaDiClaudio <andreadicla@gmail.com>
2023-02-28 09:58:26 +0100

    Improvements

--- 04._Time_To_Deploy/public/Activities/activities.js
+++ 04._Time_To_Deploy/public/Activities/activities.js
@@ -1,11 +1,18 @@
 "use strict";
-
+/*
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
+*/
 
-/**
- * @param {Date} date
- */
+const activityWrapper = document.getElementById("activity-wrapper")
+const timeWrapper = document.getElementById("time-wrapper");
+const dateWrapper = document.getElementById("date-wrapper");
+
+const addActivityButton = document.getElementById("add-activity-button");
+/*
+
+@param {Date} date
+ 
 function formatTime(date) {
   const hours12 = date.getHours() % 12 || 12;
   const minutes = date.getMinutes();
@@ -16,33 +23,12 @@ function formatTime(date) {
     .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
 }
 
-/**
- * @param {Date} date
- */
+
+//@ param {Date} date
+ 
 function formatDate(date) {
-  const DAYS = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  const MONTHS = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
+  const DAYS = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
   return `${DAYS[date.getDay()]}, ${
     MONTHS[date.getMonth()]
@@ -55,4 +41,34 @@ setInterval(() => {
   timeElement.textContent = formatTime(now);
   dateElement.textContent = formatDate(now);
 }, 200);
+*/
 
+addActivityButton.addEventListener('click', addActivity)
+
+function addActivity(){
+
+  const dateTimeWrapper = document.createElement("div");
+  dateTimeWrapper.className = "datetime"
+
+  const dateWrapper = document.createElement("div")
+  dateWrapper.className = "date";
+  const date = document.createElement("p")
+
+  const timeWrapper = document.createElement("div")
+  timeWrapper.className = "time";
+  const time = document.createElement("p")
+
+  const description = document.createElement("p")
+
+  date.innerText = document.getElementById("date-input").value;
+  time.innerText = document.getElementById("time-input").value;
+  //description.innerText(document.getElementById("description-input").value)
+
+  dateWrapper.appendChild(date);
+  timeWrapper.appendChild(time);
+
+  dateTimeWrapper.appendChild(dateWrapper);
+  dateTimeWrapper.appendChild(timeWrapper);
+
+  activityWrapper.appendChild(dateTimeWrapper);
+}
