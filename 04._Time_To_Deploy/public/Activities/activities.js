895183479ca4cb1a13bc33b9623e107ce74bb6b6
andreaDiClaudio <andreadicla@gmail.com>
2023-02-28 10:53:32 +0100

    Solved Exercise

--- 04._Time_To_Deploy/public/Activities/activities.js
+++ 04._Time_To_Deploy/public/Activities/activities.js
@@ -1,47 +1,10 @@
 "use strict";
-/*
-const timeElement = document.querySelector(".time");
-const dateElement = document.querySelector(".date");
-*/
 
-const activityWrapper = document.getElementById("activity-wrapper")
+const activityWrapper = document.getElementById("activity-wrapper");
 const timeWrapper = document.getElementById("time-wrapper");
 const dateWrapper = document.getElementById("date-wrapper");
 
 const addActivityButton = document.getElementById("add-activity-button");
-/*
-
-@param {Date} date
- 
-function formatTime(date) {
-  const hours12 = date.getHours() % 12 || 12;
-  const minutes = date.getMinutes();
-  const isAm = date.getHours() < 12;
-
-  return `${hours12.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
-}
-
-
-//@ param {Date} date
- 
-function formatDate(date) {
-  const DAYS = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  const MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-  return `${DAYS[date.getDay()]}, ${
-    MONTHS[date.getMonth()]
-  } ${date.getDate()} ${date.getFullYear()}`;
-}
-
-setInterval(() => {
-  const now = new Date();
-
-  timeElement.textContent = formatTime(now);
-  dateElement.textContent = formatDate(now);
-}, 200);
-*/
 
 function formatDate(date){
   const stringDate = date.toString();
@@ -78,6 +41,7 @@ function addActivity(){
   //Time
   const timeWrapper = document.createElement("div");
   const time = document.createElement("h1");
+  time.style.color = `${document.getElementById("color-input").value}`
 
   //Description
   const descriptionWrapper = document.createElement("div");
@@ -97,4 +61,13 @@ function addActivity(){
   dateTimeWrapper.appendChild(descriptionWrapper);
 
   activityWrapper.appendChild(dateTimeWrapper);
+
+  emptyInputs();
+}
+
+function emptyInputs(){
+  document.getElementById("time-input").value = "";
+  document.getElementById("description-input").value = "";
+  document.getElementById("date-input").value = "";
+  document.getElementById("color-input").value = "";
 }
