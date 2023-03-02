5e74624a42305afaae2e47c31c10c98d24b1c3f6
andreaDiClaudio <andreadicla@gmail.com>
2023-03-02 11:32:17 +0100

    Added post method to save the Activites and then on load read the activity saved in the simulated database

--- 04._Time_To_Deploy/app.js
+++ 04._Time_To_Deploy/app.js
@@ -1,14 +1,48 @@
 "use strict";
 
+const activities = [];
+
+let currentId = 0;
+
 const express = require("express");
 const app = express();
-
 app.use(express.static("public"));
 
+const bodyParser = require('body-parser');//Needed for post method
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))//Parses all the urlencoded bodies
+
+//HTTP Methods
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/Activities/activities.html");
 });
 
+//API
+app.get("/api/activities", (req,res) => {
+    res.send({data: activities});
+});
+
+//Post request to save new activities
+app.post("/activities", (req,res)=>{
+    const date = req.body.date;
+    const time = req.body.time;
+    const description = req.body.description;
+    const color = req.body.color;
+
+    let calculateId = ++currentId;
+    const newActivity = {
+        id: calculateId, 
+        date: date, 
+        time: time,
+        description: description,
+        color: color
+    }
+
+    console.log(description);
+    activities.push(newActivity);
+
+    res.send({data: newActivity});
+});
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
--- 04._Time_To_Deploy/public/Activities/activities.js
+++ 04._Time_To_Deploy/public/Activities/activities.js
@@ -3,8 +3,103 @@
 const activityWrapper = document.getElementById("activity-wrapper");
 const timeWrapper = document.getElementById("time-wrapper");
 const dateWrapper = document.getElementById("date-wrapper");
-
 const addActivityButton = document.getElementById("add-activity-button");
+
+addActivityButton.addEventListener('click', addActivity)
+
+window.onload= loadActivities();
+
+
+function addActivity(){
+  postActivity();
+
+  emptyInputs();
+}
+
+async function postActivity(){
+  const date =  formatDate(document.getElementById("date-input").value);
+  const time = document.getElementById("time-input").value;
+  const description = document.getElementById("description-input").value;
+  const color = document.getElementById("color-input").value;
+
+  const activity = {
+    "date": date,
+    "time": time,
+    "desc": description,
+    "color": color
+    }
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json"
+        },
+      body: ""
+      }
+
+    fetchOptions.body = JSON.stringify(activity);
+
+    const response = await fetch("http://localhost:8080/activities", fetchOptions);
+
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(errorMessage)
+    }
+    location.reload();
+}
+
+function loadActivities(){
+  fetch("http://localhost:8080/api/activities") //returns a bitstream. that is why we need to convert the response to json
+    .then(response => response.json())//Converted in Js
+    .then(result => {
+  const activityWrapper = document.getElementById("activity-wrapper");
+
+  result.data.forEach(activity => {
+        //const formattedDate = formatDate(document.getElementById("date-input").value);
+
+    //Wrapper
+    const dateTimeWrapper = document.createElement("div");
+    dateTimeWrapper.className = "datetime";
+    dateTimeWrapper.style.borderRight = `10px ${activity.color} solid`;
+
+    //date
+    const dateWrapper = document.createElement("div");
+    const date = document.createElement("h2");
+
+    //Time
+    const timeWrapper = document.createElement("div");
+    const time = document.createElement("h1");
+    time.style.color = activity.color
+
+    //Description
+    const descriptionWrapper = document.createElement("div");
+    const description = document.createElement("p");
+
+    //fill fields
+    date.innerText = activity.date;
+    time.innerText = activity.time;
+    description.innerText = activity.description;
+
+    //appendChild
+    dateWrapper.appendChild(date);
+    timeWrapper.appendChild(time);
+    descriptionWrapper.appendChild(description);
+
+    dateTimeWrapper.appendChild(timeWrapper);
+    dateTimeWrapper.appendChild(dateWrapper);
+    dateTimeWrapper.appendChild(descriptionWrapper);
+
+    activityWrapper.appendChild(dateTimeWrapper);
+    })
+  });
+}
+
+function emptyInputs(){
+  document.getElementById("time-input").value = "";
+  document.getElementById("description-input").value = "";
+  document.getElementById("date-input").value = "";
+  document.getElementById("color-input").value = "";
+}
 
 function formatDate(date){
   const stringDate = date.toString();
@@ -13,59 +108,7 @@ function formatDate(date){
   const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-  const finalDate = (`${arr[2]} , ${months[arr[1].replace(/^0+/, '')] } ${arr[0]}`)
+  const finalDate = (`${arr[2]} , ${months[arr[1].replace(/^0+/, '') - 1] } ${arr[0]}`)
   
   return finalDate;
-
-}
-
-addActivityButton.addEventListener('click', addActivity)
-
-function addActivity(){
-
-  //Formates date
-  const formattedDate = formatDate(document.getElementById("date-input").value);
-
-  //Wrapper
-  const dateTimeWrapper = document.createElement("div");
-  dateTimeWrapper.className = "datetime";
-  dateTimeWrapper.style.borderRight = `10px ${document.getElementById("color-input").value} solid`;
-
-  //date
-  const dateWrapper = document.createElement("div");
-  const date = document.createElement("h2");
-
-  //Time
-  const timeWrapper = document.createElement("div");
-  const time = document.createElement("h1");
-  time.style.color = `${document.getElementById("color-input").value}`
-
-  //Description
-  const descriptionWrapper = document.createElement("div");
-  const description = document.createElement("p");
-
-  //fill fields
-  date.innerText = formattedDate;
-  time.innerText = document.getElementById("time-input").value;
-  description.innerText = document.getElementById("description-input").value;
-
-  //appendChild
-  dateWrapper.appendChild(date);
-  timeWrapper.appendChild(time);
-  descriptionWrapper.appendChild(description);
-
-  dateTimeWrapper.appendChild(timeWrapper);
-  dateTimeWrapper.appendChild(dateWrapper);
-  dateTimeWrapper.appendChild(descriptionWrapper);
-
-  activityWrapper.appendChild(dateTimeWrapper);
-
-  emptyInputs();
-}
-
-function emptyInputs(){
-  document.getElementById("time-input").value = "";
-  document.getElementById("description-input").value = "";
-  document.getElementById("date-input").value = "";
-  document.getElementById("color-input").value = "";
 }
