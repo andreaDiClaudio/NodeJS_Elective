2e264420dbedb589f8d89372b648e6344ae08e89
andreaDiClaudio <andreadicla@gmail.com>
2023-03-03 11:47:36 +0100

    Created basic get routes

--- 05._Upper/app.js
+++ 05._Upper/app.js
@@ -5,6 +5,18 @@ const app = express();
 //const jokes = require("./util/jokes");
 //import jokes from "./util/jokes.js";//gives a experimental warning
 
+//serving static is when the browsers need an extra file etc.
+
+import path from "path";
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+});
+
+app.get("/IRLQuests", (req, res) => {
+    res.sendFile(path.resolve("public/pages/IRLQuests/IRLQuests.html"))
+});
+
 
 const PORT = 8080;
 app.listen(PORT, (error) =>{
