d388b34bb1824581d8a699e1eb5e64ddeb56b020
andreaDiClaudio <andreadicla@gmail.com>
2023-03-10 09:00:52 +0100

    CLient side redirection

--- 03._Plain_HTML_Server/public/frontpage/frontpage.js
+++ 03._Plain_HTML_Server/public/frontpage/frontpage.js
@@ -1,1 +1,5 @@
 console.log("Thank you for visiting");
+
+//Not use setInterval because it calls the callback every interval
+//setTimeout intstead sets the timout and when it is executed, it will stop
+setTimeout(() => window.location.href = "/visitors", 3000);
